afc365d1be524f2531c92230e431958a1f192f83
Taran Arora <itstaranarora@gmail.com>
2024-05-02 00:24:19 +0530

    added api call

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,56 @@ import { useState } from "react";
 export default function App() {
   const data = [
     {
+      id: "one",
       name: "Starry Night",
       image: "./Starry_Night_Thumbnail.png",
     },
     {
+      id: "two",
       name: "Brand",
       image: "./Brand thumbnail.png",
     },
     {
+      id: "three",
       name: "Forrest Kiosk ",
       image: "./Forrest Kiosk Thumbnail.png",
     },
     {
+      id: "four",
       name: "OceanView Thumbnail",
       image: "./OceanView Thumbnail.png",
     },
     {
+      id: "five",
       name: "Road Thumbnail",
       image: "./Road Thumbnail@2.png",
     },
     {
+      id: "six",
       name: "City K",
       image: "./City K.png",
     },
   ];
 
   const [selected, setSelected] = useState(data[0]);
+
+  function handleClick(e) {
+    setSelected(e);
+    const url = `localhost:3000/scenes`;
+    fetch(`${url}/${e.id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   return (
     <div
@@ -56,8 +80,8 @@ export default function App() {
       <div className="mt-6 p-10 grid grid-cols-1 relative z-20 sm:grid-cols-2 md:grid-cols-2 gap-10">
         {data.map((item) => (
           <img
-            onClick={() => setSelected(item)}
-            key={item.name}
+            onClick={() => handleClick(item)}
+            key={item.id}
             src={item.image}
             alt="sample image"
             className={`w-full object-contain ${
